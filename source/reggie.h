3500f524d70d993ff6bd22df6c0ba47eeba20717
Brandon <tricksterguy@live.com>
2014-11-22 01:39:23 -0800

    first commit

---
+++ source/reggie.h
@@ -0,0 +1,28 @@
+/*
+ * Exported with brandontools v1.0
+ * Invocation command was brandontools -mode3 -device=3ds reggie reggie.bmp 
+ * Time-stamp: Saturday 11/22/2014, 01:19:36
+ * 
+ * Image Information
+ * -----------------
+ * reggie.bmp 240@400
+ * 
+ * Quote/Fortune of the Day!
+ * -------------------------
+ * Duct tape is like the force.  It has a light side, a dark side, and it holds the universe together.  ~Carl Zwanzig
+ * 
+ * Good to begin well, better to end well.
+ * 
+ * All bug reports / feature requests are to be sent to Brandon (brandon.whitehead@gatech.edu)
+ */
+
+#ifndef REGGIE_BITMAP_H
+#define REGGIE_BITMAP_H
+
+extern const unsigned char reggie[288000];
+#define REGGIE_SIZE 288000
+#define REGGIE_WIDTH 240
+#define REGGIE_HEIGHT 400
+
+#endif
+
